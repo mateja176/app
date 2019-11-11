c182f6049ff7459173efe35720148d8984bc40c0
mateja <mateja176@gmail.com>
2019-11-11 19:55:54 +0100

    feat: initial commit

---
+++ src/About.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export interface HomeProps {}
+
+const About: React.FC<HomeProps> = () => <div>About</div>;
+
+export default About;
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Suspense } from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
 import './App.css';
+
+const HomeLazy = React.lazy(() =>
+  import('./Home').then(
+    imp => new Promise(resolve => setTimeout(resolve, 2000, imp)),
+  ),
+);
+const AboutLazy = React.lazy(() =>
+  import('./About').then(
+    imp => new Promise(resolve => setTimeout(resolve, 2000, imp)),
+  ),
+);
+
+const loading = 'Loading...';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route
+          path="/home"
+          render={() => (
+            <Suspense fallback={loading}>
+              <HomeLazy />
+            </Suspense>
+          )}
+        />
+        <Route
+          path="/about"
+          render={() => (
+            <Suspense fallback={loading}>
+              <AboutLazy />
+            </Suspense>
+          )}
+        />
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
